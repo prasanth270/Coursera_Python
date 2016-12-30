758bacce5788eeb22a871ecefa4ea29ea78b0be5
Prasanth Ramineni <prasanth270@gmail.com>
2016-12-30 15:52:02 -0800

    Lession 3 Solutions

--- problem3_6starter.py
+++ problem3_6starter.py
@@ -2,4 +2,14 @@
 
 import sys
 
-# add your code here
+
+infile = open(sys.argv[1])
+outfile = open(sys.argv[2], 'w')
+
+for line in infile:
+    line = line.strip("\n")
+    outfile.write(str(len(line)))
+    outfile.write("\n")
+    
+infile.close()
+outfile.close()
